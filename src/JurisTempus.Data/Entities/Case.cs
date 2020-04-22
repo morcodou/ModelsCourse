67b05535e898c45e407149feb5eb2678e95a5db0
morcodou <morcodou@home>
2020-04-21 21:23:51 -0400

    adding validation at the schema level

--- src/JurisTempus.Data/Entities/Case.cs
+++ src/JurisTempus.Data/Entities/Case.cs
@@ -1,17 +1,19 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace JurisTempus.Data.Entities
 {
-  public class Case
-  {
-    public int Id { get; set; }
-    public string FileNumber { get; set; }
-    public CaseStatus Status { get; set; }
+    public class Case
+    {
+        public int Id { get; set; }
+        //[Required]
+        public string FileNumber { get; set; }
+        public CaseStatus Status { get; set; }
 
-    public Client Client { get; set; }
-  }
+        public Client Client { get; set; }
+    }
 }
